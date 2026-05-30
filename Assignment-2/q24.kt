de4f5a45c8e8c9efc66863fd99e3487245d19117
DIVYA SAMARPAN DAS <gopalsdas2006@gmail.com>
2026-05-30 14:58:06 +0530

    Add File class and file creation functionality
    Implement File class and buildFile function to create files with permissions.

---
+++ Assignment-2/q24.kt
@@ -0,0 +1,21 @@
+class File(val filename: String) {
+    var permissions = ""
+}
+
+fun buildFile(filename: String): File {
+
+    return File(filename)
+        .apply {
+            permissions = "Read/Write"
+        }
+        .also {
+            println("File $filename created")
+        }
+}
+
+fun main() {
+    val file = buildFile("notes.txt")
+
+    println(file.filename)
+    println(file.permissions)
+}
